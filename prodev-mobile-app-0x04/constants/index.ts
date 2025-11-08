83fafc52774f335656a4d5c6a22f658e1188c3e3
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-08 15:55:29 +0100

    new logo

--- prodev-mobile-app-0x04/constants/index.ts
+++ prodev-mobile-app-0x04/constants/index.ts
@@ -3,5 +3,4 @@ const HEROLOGO = require('@/assets/images/logo.png')
 const HEROLOGOGREEN = require("@/assets/images/logo-green.png")
 const GOOGLELOGO = require('@/assets/images/google.png')
 const FACEBOOKLOGO = require('@/assets/images/facebook.png')
-
 export { BACKGROUNDIMAGE, HEROLOGO, HEROLOGOGREEN, GOOGLELOGO, FACEBOOKLOGO }
